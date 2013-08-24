e2080c900c26b7b1047e7f6ad07e64016e27515f
Matthew Kalch <mfkalch@gmail.com>
2013-08-24 07:57:36 +0000

    Added setup.sh

---
+++ setup.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+# Simple setup.sh for configuring Ubuntu 12.04 LTS EC2 instance
+# for headless setup. 
+
+# Update and upgrade apt-get and 
+# install gcc, g++ and Make toolchain and compilers.
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get install build-essential
+
+#Install emacs24
+# https://launchpad.net/~cassou/+archive/emacs
+sudo apt-add-repository -y ppa:cassou/emacs
+sudo apt-get -qq update
+sudo apt-get install -y emacs24-nox emacs24-el emacs24-common-non-dfsg
+
+# git pull and install dotfiles as well
+cd $HOME
+if [ -d ./dotfiles/ ]; then
+    mv dotfiles dotfiles.old
+fi
+if [ -d .emacs.d/ ]; then
+    mv .emacs.d .emacs.d~
+fi
+git clone https://github.com/mfkalch/dotfiles.git
+ln -sb dotfiles/.screenrc .
+ln -sb dotfiles/.bash_profile .
+ln -sb dotfiles/.bashrc .
+ln -sb dotfiles/.bashrc_custom .
+ln -sf dotfiles/.emacs.d .
+
+# Added git global config for user
+git config --global user.name "Matthew Kalch"
+git config --global user.email "mfkalch@gmail.com"
